418192a05164d03c08dc5ed41238591bd56929fd
sachincse-blip <sachingacse@gmail.com>
2026-02-20 13:00:08 +0530

    Create static and non static methods.java

---
+++ static and non static methods.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    void display1()
+    {
+        System.out.println("HEYY INGA PAARUU DA");
+    }
+    static void display2()
+    {
+        System.out.println("INTHA SILENT AH APDIYEA MAINTAIN PANNU");
+    }
+	public static void main(String[] args) {
+		Main obj=new Main();
+		obj.display1();
+		display2();
+	}
+}
