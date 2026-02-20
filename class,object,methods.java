b2448d94ae110f863bc93e63f09528f18a28f6de
sachincse-blip <sachingacse@gmail.com>
2026-02-20 13:00:56 +0530

    Create class,object,methods.java

---
+++ class,object,methods.java
@@ -0,0 +1,24 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    CSEA obj1=new CSEA();
+	    obj1.display();
+	    CSEB obj2=new CSEB();
+	    obj2.display();
+	}
+}
+class CSEA 
+{
+    void display()
+    {
+     System.out.println("CSEA STUDENTS ARE VERY Smart");
+    }
+}
+
+class CSEB
+{
+    void display()
+    {
+     System.out.println("CSEB STUDENTS ARE VERY NICE");
+    }
+}
