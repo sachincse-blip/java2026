6ded505a0dc72f99733503681cd49d5423ff2da1
sachincse-blip <sachingacse@gmail.com>
2026-02-20 12:58:34 +0530

    Create methods to call the paremeters.java

---
+++ methods to call the paremeters.java
@@ -0,0 +1,20 @@
+public class Main
+{
+    static void add(int x, int y)
+    {
+        System.out.println(x+y);
+    }
+    static void sub(int x,int y)
+    {
+         System.out.println(x-y);
+    }
+    static void mul(int x,int y)
+    {
+        System.out.println(x*y);
+    }
+	public static void main(String[] args) {
+	    int a=10 , b=20;
+	    add(a,b);
+	    mul(a,b);
+	}
+}
